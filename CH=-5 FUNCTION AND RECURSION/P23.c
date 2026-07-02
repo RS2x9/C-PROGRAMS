7f42dcbdfb7f347bd21461d71014e214e16929a5
RS2x9 <162338837+RS2x9@users.noreply.github.com>
2026-07-03 00:22:31 +0530

    Update P23.c

--- CH=-5 FUNCTION AND RECURSION/P23.c
+++ CH=-5 FUNCTION AND RECURSION/P23.c
@@ -1,44 +1,18 @@
 // HOMEWORK SET
 // Q1) write a function to find sum of digits of a number
 // doing this problem with recursion
+
 #include<stdio.h>
-
-int sums(int n);
-
+int sum( int num)
+{
+    if ( num == 0) return 0;
+    return ( num%10) + sum( num /10);
+}
 int main()
 {
-    int n;
-    printf("Enter number:");
-    scanf("%d",& n);
-    printf("sum is :%d",sums(n));
+    int n ;
+    printf("E: ");
+    scanf("%d", &n);
+    printf( "%d" , sum(n));
     return 0;
-}
-
-int sums(int n)
-{
-    int a,sum=0;
-    a=n%10;  // here awill come <10, always 
-    n=n/10;
-    sum=sum+a;
-    if (n<10)
-    {
-        sum=sum+n;
-        return sum;
-    }
-
-    else 
-    {
-        do
-        {
-            a=n%10;
-            n=n/10;
-            sum=sum+a;
-            if (n<10)
-            {
-                sum=sum+n;
-                return sum;
-            }
-        }
-        while(n>10);
-    }
 }
